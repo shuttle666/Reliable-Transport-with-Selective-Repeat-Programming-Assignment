6d9a4df31cca9a6c321b8eaf41ff6528d1ca91af
shuttle666 <44249819+shuttle666@users.noreply.github.com>
2025-05-03 21:17:24 +0930

    Modify the receiving and timeout logic at the sending end to achieve selective confirmation and retransmission. The A_input is changed to selective confirmation. After receiving the ACK, only the corresponding package is marked as confirmed. Change "A_timerinterrupt" to selective retransmission.

--- sr.c
+++ sr.c
@@ -41,42 +41,73 @@ bool IsCorrupted(struct pkt packet)
 
 /********* Sender (A) variables and functions ************/
 
-void A_output(struct msg message)
+void A_input(struct pkt packet)
 {
-  struct pkt sendpkt;
   int i;
 
-  if (windowcount < WINDOWSIZE)
+  if (!IsCorrupted(packet))
   {
-    if (TRACE > 1)
-      printf("----A: New message arrives, send window is not full, send new messge to layer3!\n");
-
-    sendpkt.seqnum = A_nextseqnum;
-    sendpkt.acknum = NOTINUSE;
-    for (i = 0; i < 20; i++)
-      sendpkt.payload[i] = message.data[i];
-    sendpkt.checksum = ComputeChecksum(sendpkt);
-
-    windowlast = (windowlast + 1) % WINDOWSIZE;
-    buffer[windowlast].packet = sendpkt;
-    buffer[windowlast].sent = 1;
-    buffer[windowlast].acked = 0;
-    windowcount++;
-
     if (TRACE > 0)
-      printf("Sending packet %d to layer 3\n", sendpkt.seqnum);
-    tolayer3(A, sendpkt);
+      printf("----A: uncorrupted ACK %d is received\n", packet.acknum);
+    total_ACKs_received++;
 
-    if (windowcount == 1)
-      starttimer(A, RTT);
+    if (windowcount != 0)
+    {
+      /* Find the packet in the window with the matching seqnum */
+      int acked_idx = -1;
+      for (i = 0; i < windowcount; i++)
+      {
+        int idx = (windowfirst + i) % WINDOWSIZE;
+        if (buffer[idx].packet.seqnum == packet.acknum)
+        {
+          acked_idx = idx;
+          break;
+        }
+      }
 
-    A_nextseqnum = (A_nextseqnum + 1) % MAX_SEQ;
+      if (acked_idx != -1 && buffer[acked_idx].acked == 0)
+      {
+        if (TRACE > 0)
+          printf("----A: ACK %d is not a duplicate\n", packet.acknum);
+        new_ACKs++;
+
+        /* Mark the packet as acked */
+        buffer[acked_idx].acked = 1;
+
+        /* Slide window if possible (move windowfirst to the first unacked packet) */
+        while (windowcount > 0 && buffer[windowfirst].acked == 1)
+        {
+          windowcount--;
+          windowfirst = (windowfirst + 1) % WINDOWSIZE;
+        }
+
+        /* Restart timer if there are still unacked packets */
+        stoptimer(A);
+        if (windowcount > 0)
+        {
+          /* Find the first unacked packet to set the timer */
+          for (i = 0; i < windowcount; i++)
+          {
+            int idx = (windowfirst + i) % WINDOWSIZE;
+            if (buffer[idx].acked == 0)
+            {
+              starttimer(A, RTT);
+              break;
+            }
+          }
+        }
+      }
+      else
+      {
+        if (TRACE > 0)
+          printf("----A: duplicate ACK received, do nothing!\n");
+      }
+    }
   }
   else
   {
     if (TRACE > 0)
-      printf("----A: New message arrives, send window is full\n");
-    window_full++;
+      printf("----A: corrupted ACK is received, do nothing!\n");
   }
 }
 
@@ -136,17 +167,26 @@ void A_timerinterrupt(void)
   int i;
 
   if (TRACE > 0)
-    printf("----A: time out,resend packets!\n");
+    printf("----A: time out, resend unacked packets!\n");
 
+  /* Only resend packets that are not acked */
+  int timer_started = 0;
   for (i = 0; i < windowcount; i++)
   {
-    if (TRACE > 0)
-      printf("---A: resending packet %d\n", (buffer[(windowfirst + i) % WINDOWSIZE]).packet.seqnum); /* Updated to buffer[i].packet */
+    int idx = (windowfirst + i) % WINDOWSIZE;
+    if (buffer[idx].sent == 1 && buffer[idx].acked == 0)
+    {
+      if (TRACE > 0)
+        printf("---A: resending packet %d\n", buffer[idx].packet.seqnum);
 
-    tolayer3(A, buffer[(windowfirst + i) % WINDOWSIZE].packet); /* Updated to buffer[i].packet */
-    packets_resent++;
-    if (i == 0)
-      starttimer(A, RTT);
+      tolayer3(A, buffer[idx].packet);
+      packets_resent++;
+      if (!timer_started)
+      {
+        starttimer(A, RTT);
+        timer_started = 1;
+      }
+    }
   }
 }
 
